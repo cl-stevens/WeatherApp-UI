4a0a1850af185b12a0015775b6d596316a379ab5
cl-stevens <carltonstevens1202@gmail.com>
2026-02-23 17:16:54 -0500

    Initial commit

---
+++ WeatherAppUI/WeatherAppUI/WeatherAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUIApp.swift
+//  WeatherAppUI
+//
+//  Created by CJ Stevens on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppUI/WeatherAppUITests/WeatherAppUITests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppUITests.swift
+//  WeatherAppUITests
+//
+//  Created by CJ Stevens on 2/23/26.
+//
+
+import Testing
+@testable import WeatherAppUI
+
+struct WeatherAppUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
